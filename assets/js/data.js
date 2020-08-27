7ffadd769023d4a51b9c9cd2c2b23a25bb9f6a04
cai <1375055073@qq.com>
2020-08-27 15:39:14 +0800

    feat: init

---
+++ assets/js/data.js
@@ -0,0 +1,214 @@
+export default {
+  header: {
+    companyName: '赛尔网络有限公司',
+    companyDesc: '网络服务教育 创新开拓未来',
+    shortVideo: {
+      url:
+        'https://2726-cn-north-4.cdn-vod.huaweicloud.com/asset/81d32a637a9a58ed0115c5e1ff539cc2/b467c809206f08005f2182f9b102b11d.mp4',
+      type: 'video/mp4',
+    },
+    originVideo: {
+      url:
+        'https://2726-cn-north-4.cdn-vod.huaweicloud.com/asset/ec0b0f66e2992cf5d404c7f982bdce63/3e6925f60d37fd3e89d3b02fff6607e0.mp4',
+      type: 'video/mp4',
+    },
+    // logo 反白 480 x 120 透明底 png
+    logoWhiteUrl: require('@/assets/imgs/logo.png'),
+    // logo 彩色 480 x 120 透明底 png（本页面不需要，但要提供，用于网安周官网的企业虚拟展二级页面使用）
+    logoColorUrl: require('@/assets/imgs/logos.png'),
+  },
+  news: {
+    title: '资讯',
+    'title-en': 'NEWS',
+    list: [
+      {
+        title: '6NOC2020年上半年报：CERNET2出口入流量峰值13.24Gbps',
+        desc: '',
+        date: '2020-08-14',
+        // 480 x 320
+        picUrl: require('@/assets/imgs/p1-480x320.jpg'),
+        link:
+          'https://www.edu.cn/xxh/cernet/6noc/202008/t20200814_1751538.shtml',
+      },
+      {
+        title: '广东省教育厅：广东率先实现高校edu.cn域名全覆盖',
+        desc: '',
+        date: '2020-08-12',
+        picUrl: require('@/assets/imgs/p2-480x320.jpg'),
+        link: 'https://www.edu.cn/xxh/focus/df/202008/t20200812_1751138.shtml',
+      },
+      {
+        title: '全球首份“推进IPv6规模部署向纯IPv6发展联合倡议”',
+        desc: '',
+        date: '2020-07-30',
+        picUrl: require('@/assets/imgs/p3-480x320.jpg'),
+        link: 'https://www.edu.cn/info/yc/202007/t20200730_1749343.shtml',
+      },
+      {
+        title: 'CERNET：三大战役护航高校应对疫情',
+        desc: '',
+        date: '2020-07-24',
+        picUrl: require('@/assets/imgs/p4-480x320.jpg'),
+        link: 'https://www.edu.cn/info/yc/202007/t20200724_1740097.shtml',
+      },
+      {
+        title: '疫情下的教育和网络',
+        desc: '',
+        date: '2020-04-20',
+        picUrl: require('@/assets/imgs/p5-480x320.jpg'),
+        link: 'https://www.edu.cn/xxh/yc/202004/t20200420_1722369.shtml',
+      },
+      {
+        title:
+          '教育部：CERNET发布全球学术资源共享平台 支持高校疫情防控科研攻关',
+        desc: '',
+        date: '2020-03-09',
+        picUrl: require('@/assets/imgs/p6-480x320.jpg'),
+        link: 'https://www.edu.cn/info/zt/fk/202003/t20200309_1715656.shtml',
+      },
+      {
+        title: 'CERNET第二十六届学术年会暨会员代表大会在杭州胜利闭幕',
+        desc: '',
+        date: '2019-11-14',
+        picUrl: require('@/assets/imgs/p7-480x320.jpg'),
+        link:
+          'https://www.cernet.edu.cn/xxh/zt/26cernet/dt/201911/t20191114_1693140.shtml',
+      },
+      {
+        title: '雷朝滋：CERNET要为教育强国、网络强国做出更大贡献',
+        desc: '',
+        date: '2019-11-12',
+        picUrl: require('@/assets/imgs/p8-480x320.jpg'),
+        link:
+          'https://www.edu.cn/xxh/zt/26cernet/dt/201911/t20191112_1692539.shtml',
+      },
+    ],
+  },
+  pictures: {
+    title: '图片',
+    'title-en': 'PICTURES',
+    list: [
+      {
+        // 564 x 376
+        picUrl: require('@/assets/imgs/P1-564x376.jpg'),
+      },
+      {
+        picUrl: require('@/assets/imgs/P2-564x376.jpg'),
+      },
+      {
+        picUrl: require('@/assets/imgs/P3-564x376.jpg'),
+      },
+      {
+        picUrl: require('@/assets/imgs/P4-564x376.jpg'),
+      },
+      {
+        picUrl: require('@/assets/imgs/P5-564x376.jpg'),
+      },
+      {
+        picUrl: require('@/assets/imgs/P6-564x376.jpg'),
+      },
+      {
+        picUrl: require('@/assets/imgs/P7-564x376.jpg'),
+      },
+      {
+        picUrl: require('@/assets/imgs/P8-564x376.jpg'),
+      },
+    ],
+  },
+  videos: {
+    title: '视频',
+    'title-en': 'VIDEOS',
+    list: [
+      {
+        title: '赛尔网络大事记',
+        time: '00:03:59',
+        // 660 x 380
+        picUrl: require('@/assets/imgs/p1-660x380.png'),
+        video: {
+          url:
+            'https://2726-cn-north-4.cdn-vod.huaweicloud.com/asset/a42e0ac399cb4ee2244a65a45ec2ee67/f265df8dd367fd9c8f80df9565c20013.mp4',
+          type: 'video/mp4',
+        },
+      },
+      {
+        title: '赛尔网络三大服务体系',
+        time: '00:01:01',
+        picUrl: require('@/assets/imgs/p2-660x380.png'),
+        video: {
+          url:
+            'https://2726-cn-north-4.cdn-vod.huaweicloud.com/asset/9e44f44e449ad201bc5649f16bb0ff51/2f9a42a7d1c097951cb2cffc995a62ea.mp4',
+          type: 'video/mp4',
+        },
+      },
+      {
+        title: '赛尔网络获得多项国家荣誉',
+        time: '00:01:39',
+        picUrl: require('@/assets/imgs/p3-660x380.png'),
+        video: {
+          url:
+            'https://2726-cn-north-4.cdn-vod.huaweicloud.com/asset/bff6de887dcbf59a57a10b3a72281a15/1dd1b21aac811c95080a3617e88e75f3.mp4',
+          type: 'video/mp4',
+        },
+      },
+      {
+        title: '赛尔网络助力高考招生录取工作',
+        time: '00:01:09',
+        picUrl: require('@/assets/imgs/p4-660x380.png'),
+        video: {
+          url:
+            'https://2726-cn-north-4.cdn-vod.huaweicloud.com/asset/337eeffb22a84c611f7b0e0444e312f6/480bed572fcb7f47f54d1c99336a3264.mp4',
+          type: 'video/mp4',
+        },
+      },
+    ],
+  },
+  about: {
+    introduction: [
+      '赛尔网络有限公司成立于2000年12月，经教育部批准，负责中国教育和科研计算机网CERNET的运营。',
+      '随着国家对网信事业的日益重视，CERNET作为教育信息化基础设施和建设平台的作用愈加重要。作为CERNET主干网运行者，赛尔网络秉承“修己奉献 敬事创新”的核心价值观，致力于完善CERNET网络服务体系、扩大CERNET网络覆盖范围、提升CERNET网络运行质量与服务水平，努力推动教育信息化，振兴中国教育事业，创造互联网美好未来。',
+      '赛尔网络立足教育、服务社会，努力做好CERNET服务，形成了网络服务、技术服务、信息服务、产业服务四大业务体系；设立了网络运行中心和客户服务中心，在全国范围内组建了31家分公司，形成了网络运行、客户服务、营销平台三位一体的综合服务能力；发展了赛尔互联、赛尔新技术、金智华教、赛尔投资、赛尔教育、赛尔时代传媒与下一代互联网重大应用技术工程研究中心等多家子公司。',
+    ],
+    booths: [
+      {
+        picUrl: require('@/assets/imgs/intro-1.jpg'),
+      },
+      {
+        picUrl: require('@/assets/imgs/intro-2.jpg'),
+      },
+      // {
+      //   title:
+      //     '这是视频标题这是视频标题这是视频标题，最多显示一行超出部分则频标题，最多显示一行超频标题，最多显示一行超这是视频标题这是视频标题这是视频标题，最多显示一行超出部分则频标题，最多显示一行超频标题，最多显示一行超',
+      //   // 1200 x 600
+      //   picUrl: require('@/assets/imgs/P1-1200x600.png'),
+      //   time: '01:13:00',
+      //   video: {
+      //     url:
+      //       '//static.gdzwfw.gov.cn/portal/version-introduction/media/version-introduction-short.mp4',
+      //     type: 'video/mp4',
+      //   },
+      //   type: 'video',
+      // },
+      // {
+      //   title: '这是视频标题这是视频标题这是视频标题',
+      //   picUrl: require('@/assets/imgs/P2-1200x600.png'),
+      //   time: '01:13:00',
+      //   video: {
+      //     url:
+      //       '//static.gdzwfw.gov.cn/portal/version-introduction/media/version-introduction-short.mp4',
+      //     type: 'video/mp4',
+      //   },
+      //   type: 'video',
+      // },
+      // {
+      //   title: '这是视频标题这是视频标题',
+      //   picUrl: require('@/assets/imgs/P3-1200x600.png'),
+      //   type: 'image',
+      // },
+      // {
+      //   title: '这是视频标题这是视频标题',
+      //   picUrl: require('@/assets/imgs/P4-1200x600.png'),
+      //   type: 'image',
+      // },
+    ],
+  },
+}
